d30f1aaacbe91dcb27d79bb3686f23f471bd36bf
aboobackerj <jasim.aboobacker@kpn.com>
2021-10-17 12:32:06 +0200

    Removing all the default apis and setting the sample api details

--- src/main/java/com/distance/calculator/CalculatorApplication.java
+++ src/main/java/com/distance/calculator/CalculatorApplication.java
@@ -2,7 +2,16 @@ package com.distance.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -12,4 +21,26 @@ public class CalculatorApplication {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration(){
+		return  new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/distance-calculator/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.distance.calculator"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails(){
+		return  new ApiInfo(
+				"Distance calculator API",
+				"Sample API for WoodWing",
+				"1.0",
+				"Free to use",
+				new Contact("Jasim Aboobacker","https://test.com","jasim.aboobacker@gmail.com"),
+				"ÄPI License",
+				"http://test.com",
+				Collections.emptyList()
+		);
+	}
 }
